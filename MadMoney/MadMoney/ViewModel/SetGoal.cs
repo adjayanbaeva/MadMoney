2618bb20535df5ff8bb130d3d184b8039c6d1770
adjayanbaeva <adjayanbaeva@gmail.com>
2020-03-20 21:54:09 -0700

    msg after merging

---
+++ MadMoney/MadMoney/ViewModel/SetGoal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MadMoney.ViewModel
+{
+    public class SetGoal
+    {
+        public decimal Goal { get; set; }
+
+        public string goalMonth { get; set; }
+
+
+        public void AddGoal(string goal, string goalMonth)
+        {
+           
+            
+        }
+
+    
+}
