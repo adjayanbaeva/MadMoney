391deb22b124cb22c3c03eeb1f2864ef558bc946
Brillan <46096098+bpm150@users.noreply.github.com>
2020-03-22 16:00:56 -0700

    MainPage: Reset the binding context OnAppearing

--- MadMoney/MadMoney/MainBudgetSummaryPage.xaml.cs
+++ MadMoney/MadMoney/MainBudgetSummaryPage.xaml.cs
@@ -13,7 +13,10 @@ namespace MadMoney
     [DesignTimeVisible(false)]
     public partial class MainBudgetSummaryPage : ContentPage
     {
-        // May not need to retain a reference to the ViewModel instance
+        // Storage for the viewmodel object
+        // Doing this instead of (MainBudgetSummaryPageViewModel)BindingContext
+        // for readability since we will need a temp var for it eventually
+        // anyway in OnAppearing
         private MainBudgetSummaryPageViewModel ViewModel = null;
 
         public MainBudgetSummaryPage()
@@ -49,8 +52,9 @@ namespace MadMoney
                             ExpenseCategory.Enum.Healthcare);
 
 
-            // May not need to retain a reference to the ViewModel instance
-            BindingContext = ViewModel = new MainBudgetSummaryPageViewModel();
+            // Save reference to the viewmodel instance
+            // will remove it and re-add it OnAppearing
+            ViewModel = new MainBudgetSummaryPageViewModel();
 
 
 
@@ -63,15 +67,25 @@ namespace MadMoney
         }
 
 
-        private void PreviousMonthButton_Pressed(object sender, EventArgs e)
+        // Whenever this page is loaded, and also every time this page is
+        // navigated to from another page (such as when a page above it on
+        // the stack is popped)
+        protected override void OnAppearing()
         {
-            ViewModel.LoadPreviousMonth();
+            base.OnAppearing();
+            ResetBindingContext();
+        }
+
+        private void ResetBindingContext()
+        {
 
             // Clear the BindingContext for the MainPage, then set it back to
             // the same ViewModel. Goal is to force the UI to re-query all
             // the properties that it is bound to on the viewmodel.
+
             BindingContext = null;
             BindingContext = ViewModel;
+
             // For some reason setting it to null is required, doesn't work
             // without that. I wonder why!
             // Perhaps the compiler optimizes away the re-assignment of ViewModel
@@ -81,12 +95,21 @@ namespace MadMoney
             // be static after all. Whoops! I'll attribute that design oversight
             // to me being new to using properties on classes.
 
-
             // TODO: OBSERVE AND VERIFY
             // (Does that work for getting all of the UI elements to refresh?)
             // Yes, it does.
             // Probably still the best practice to navigate back to the main
             // (That is, the MainPage navigating to itself)
+
+        }
+
+
+
+        private void PreviousMonthButton_Pressed(object sender, EventArgs e)
+        {
+            ViewModel.LoadPreviousMonth();
+
+            ResetBindingContext();
 
             // See also:
             // Budget.AddExpense() for another place that is currently
@@ -105,26 +128,8 @@ namespace MadMoney
         {
             ViewModel.LoadNextMonth();
 
-            // Clear the BindingContext for the MainPage, then set it back to
-            // the same ViewModel. Goal is to force the UI to re-query all
-            // the properties that it is bound to on the viewmodel.
-            BindingContext = null;
-            BindingContext = ViewModel;
-            // For some reason setting it to null is required, doesn't work
-            // without that. I wonder why!
-            // Perhaps the compiler optimizes away the re-assignment of ViewModel
-            // to BindingContext, as though it could not possibly have an effect?
-            // This viewmodel class type, while instantiable, has no state.
-            // Wait. If the class type truly has no state, it should probably
-            // be static after all. Whoops! I'll attribute that design oversight
-            // to me being new to using properties on classes.
 
-
-            // TODO: OBSERVE AND VERIFY
-            // (Does that work for getting all of the UI elements to refresh?)
-            // Yes, it does.
-            // Probably still the best practice to navigate back to the main
-            // (That is, the MainPage navigating to itself)
+            ResetBindingContext();
 
             // See also:
             // Budget.AddExpense() for another place that is currently
